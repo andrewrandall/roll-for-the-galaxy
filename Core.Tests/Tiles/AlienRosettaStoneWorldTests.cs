a92dc5975015dd644b9fab47c72e6982b8454abd
Drew Randall <drew.t.randall@gmail.com>
2016-10-23 10:58:35 -0500

    AlienRosettaStone

---
+++ Core.Tests/Tiles/AlienRosettaStoneWorldTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rftg.Dice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rftg.Tiles
+{
+    [TestClass]
+    public class AlienRosettaStoneWorldTests
+    {
+        Player player;
+        AlienRosettaStoneWorld tile;
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            player = new Player(new Game());
+            tile = new AlienRosettaStoneWorld();
+        }
+
+        [TestMethod]
+        public void Adds_Alien_Tech_To_Citizenry()
+        {
+            var techsBefore = CountTechs();
+
+            player.Gain(tile);
+
+            Assert.AreEqual(1, CountTechs() - techsBefore);
+        }
+
+        private int CountTechs() => player.Citizenry.Count(c => c is AlienTechnology);
+    }
+}
---
+++ Core/Tiles/AlienRosettaStoneWorld.cs
@@ -0,0 +1,19 @@
+﻿using Rftg.Dice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rftg.Tiles
+{
+    class AlienRosettaStoneWorld : Ownable, GainPower
+    {
+        public int Cost => 1;
+
+        public void GainFor(Player player)
+        {
+            player.Citizenry.Add(new AlienTechnology());
+        }
+    }
+}
